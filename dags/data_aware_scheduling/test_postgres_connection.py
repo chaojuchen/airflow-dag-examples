3d6e6eec608e77a7eb2bec95a6763d76934456db
chaojuchen <chaoju_chen@tangram-os.ai>
2025-09-16 12:37:38 +0200

    Update test_postgres_connection.py

--- dags/data_aware_scheduling/test_postgres_connection.py
+++ dags/data_aware_scheduling/test_postgres_connection.py
@@ -5,11 +5,8 @@ from airflow.hooks.base import BaseHook
 import psycopg2
 
 def test_postgres_conn():
-    # Get Airflow connection
     conn = BaseHook.get_connection("tangram_sql")
-    # Build connection string
     conn_str = f"host={conn.host} port={conn.port or 5432} dbname={conn.schema} user={conn.login} password={conn.password}"
-    # Connect and run query
     with psycopg2.connect(conn_str) as pg_conn:
         with pg_conn.cursor() as cur:
             cur.execute("SELECT version();")
@@ -19,7 +16,7 @@ def test_postgres_conn():
 with DAG(
     dag_id="test_postgres_conn_dag",
     start_date=days_ago(1),
-    schedule_interval=None,
+    schedule_interval="* * * * *",
     catchup=False,
     tags=["test"],
 ) as dag:
