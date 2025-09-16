3ad123392bf21b5187cdefd1e9ced0ad8ed39f47
chaojuchen <chaoju_chen@tangram-os.ai>
2025-09-16 12:27:41 +0200

    Update test_postgres_connection.py

--- dags/data_aware_scheduling/test_postgres_connection.py
+++ dags/data_aware_scheduling/test_postgres_connection.py
@@ -1,27 +1,29 @@
-from airflow.decorators import dag, task
+from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
-from airflow.sdk import Connection
+from airflow.hooks.base import BaseHook
 import psycopg2
 
-@dag(
-    schedule=None,
+def test_postgres_conn():
+    # Get Airflow connection
+    conn = BaseHook.get_connection("tangram_sql")
+    # Build connection string
+    conn_str = f"host={conn.host} port={conn.port or 5432} dbname={conn.schema} user={conn.login} password={conn.password}"
+    # Connect and run query
+    with psycopg2.connect(conn_str) as pg_conn:
+        with pg_conn.cursor() as cur:
+            cur.execute("SELECT version();")
+            version = cur.fetchone()
+            print("Postgres version:", version)
+
+with DAG(
+    dag_id="test_postgres_conn_dag",
     start_date=days_ago(1),
+    schedule_interval=None,
     catchup=False,
     tags=["test"],
-)
-def test_postgres_conn_dag():
-    @task
-    def test_postgres_conn():
-        # Get Airflow connection
-        conn = Connection.get("sqlmesh_postgres")
-        # Build connection string
-        conn_str = f"host={conn.host} port={conn.port or 5432} dbname={conn.schema} user={conn.login} password={conn.password}"
-        # Connect and run query
-        with psycopg2.connect(conn_str) as pg_conn:
-            with pg_conn.cursor() as cur:
-                cur.execute("SELECT version();")
-                version = cur.fetchone()
-                print("Postgres version:", version)
-    test_postgres_conn()
-
-test_postgres_conn_dag = test_postgres_conn_dag()
+) as dag:
+    test_postgres_conn_task = PythonOperator(
+        task_id="test_postgres_conn",
+        python_callable=test_postgres_conn,
+    )
