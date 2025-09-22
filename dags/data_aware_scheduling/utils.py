bb7fe6f34bc48306772ff7db2d6df06564d5478e
chaojuchen <chaoju_chen@tangram-os.ai>
2025-09-22 12:47:15 +0200

    Fix Jinja template rendering in TangramSQLExecutionOperator
    - Move SQL prepending from render_template_fields to __init__
    - Concatenate lineage info with SQL before parent initialization
    - Remove prepend_str from template_fields (no longer needed)
    - Ensure Jinja variables ({{ti.dag_id}}, {{ti.run_id}}, etc.) are properly rendered
    - Simplify implementation by removing functools.partial approach

--- dags/data_aware_scheduling/utils.py
+++ dags/data_aware_scheduling/utils.py
@@ -1,27 +1,26 @@
-import functools
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 import logging
 
 class TangramSQLExecutionOperator(SQLExecuteQueryOperator):
-    template_fields = SQLExecuteQueryOperator.template_fields + ("prepend_str",)
+    # Only sql needs to be a template field since we concatenate in __init__
+    template_fields = SQLExecuteQueryOperator.template_fields
 
     def __init__(self, *args, tangram_workspace, **kwargs):
-        self.prepend_str = f'-- {{"job":{{"rn":{{"resourceType":{{"app":{{"group":"org.apache","name":"airflow"}},"name":"TaskInstance"}},"names":["{tangram_workspace}", "{{{{ti.dag_id}}}}","{{{{ti.run_id}}}}","{{{{ti.task_id}}}}"]}}}}}}'
+        # Create the prepend string with Jinja variables
+        prepend_str = f'-- {{"job":{{"rn":{{"resourceType":{{"app":{{"group":"org.apache","name":"airflow"}},"name":"TaskInstance"}},"names":["{tangram_workspace}", "{{{{ti.dag_id}}}}","{{{{ti.run_id}}}}","{{{{ti.task_id}}}}"]}}}}}}'
+        
+        # Concatenate prepend_str with sql BEFORE calling super().__init__
+        # This way the combined string will be rendered by Airflow's templating
+        if "sql" in kwargs:
+            if isinstance(kwargs["sql"], str):
+                kwargs["sql"] = prepend_str + '\n' + kwargs["sql"]
+            elif isinstance(kwargs["sql"], list):
+                kwargs["sql"] = [prepend_str + '\n' + s for s in kwargs["sql"]]
+        
         super().__init__(*args, **kwargs)
 
-    def render_template_fields(self, context, jinja_env=None):
-        def prepend_with_newline(prepend_str, sql):
-            return prepend_str + '\n' + sql
-        prepend_func = functools.partial(prepend_with_newline, self.prepend_str)
-        # Use proper logging instead of print statements
-        logger = logging.getLogger(__name__)
-        
-        logger.info(f"Prepending SQL with Tangram lineage info")
-        logger.debug(f"Lineage info: {self.prepend_str}")
-        logger.debug(f"Original SQL: {self.sql}")
-        self.sql = prepend_func(self.sql) if isinstance(self.sql, str) else [prepend_func(s) for s in self.sql]
-
-
     def execute(self, context):
+        logger = logging.getLogger(__name__)
+        logger.info("Executing SQL with rendered Jinja variables")
         print("Rendered SQL:", self.sql)
         return super().execute(context)
