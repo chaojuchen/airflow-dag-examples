512c60de37f790e72244f1c2f52f5b19fc53c20f
chaojuchen <chaoju_chen@tangram-os.ai>
2025-09-22 11:21:25 +0200

    Fix the prepend string of nyc_monday_zone_earnings DAG

--- dags/data_aware_scheduling/utils.py
+++ dags/data_aware_scheduling/utils.py
@@ -3,7 +3,6 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 class SQLExecuteQueryOperatorWithLineage(SQLExecuteQueryOperator):
     def __init__(self, *args, tangram_workspace, **kwargs):
         super().__init__(*args, **kwargs)
-        # -- {{"job":{{"rn":{{"resourceType":{{"app":{{"group":"org.apache","name":"airflow"}},"name":"TaskInstance"}},"names":["{tangram_workspace}", "{{{{ti.dag_id}}}}","{{{{ti.run_id}}}}","{{{{ti.task_id}}}}"]}}}}}}
         prepend_str = f'-- {{"job":{{"rn":{{"resourceType":{{"app":{{"group":"org.apache","name":"airflow"}},"name":"TaskInstance"}},"names":["{tangram_workspace}", "{{{{ti.dag_id}}}}","{{{{ti.run_id}}}}","{{{{ti.task_id}}}}"]}}}}}}\n'
         print(f"Prepending SQL with Tangram lineage info: {prepend_str}")
         if isinstance(self.sql, str):
