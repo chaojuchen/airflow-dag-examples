3d04efec9e0434fcfa1961f8ccb8d2956c60fa74
chaojuchen <chaoju_chen@tangram-os.ai>
2025-09-22 11:02:41 +0200

    Fix nyc_monday_zone_earnings DAG

--- dags/data_aware_scheduling/utils.py
+++ dags/data_aware_scheduling/utils.py
@@ -4,7 +4,7 @@ class SQLExecuteQueryOperatorWithLineage(SQLExecuteQueryOperator):
     def __init__(self, *args, tangram_workspace, **kwargs):
         super().__init__(*args, **kwargs)
         # -- {{"job":{{"rn":{{"resourceType":{{"app":{{"group":"org.apache","name":"airflow"}},"name":"TaskInstance"}},"names":["{tangram_workspace}", "{{{{ti.dag_id}}}}","{{{{ti.run_id}}}}","{{{{ti.task_id}}}}"]}}}}}}
-        prepend_str = f'-- {{"job":{{"rn":{{"resourceType":{{"app":{{"group":"org.apache","name":"airflow"}},"name":"TaskInstance"}},"names":["{tangram_workspace}", "{{{{ti.dag_id}}}}","{{{{ti.run_id}}}}","{{{{ti.task_id}}}}"]}}}}}}'
+        prepend_str = f'-- {{"job":{{"rn":{{"resourceType":{{"app":{{"group":"org.apache","name":"airflow"}},"name":"TaskInstance"}},"names":["{tangram_workspace}", "{{{{ti.dag_id}}}}","{{{{ti.run_id}}}}","{{{{ti.task_id}}}}"]}}}}}}\n'
         print(f"Prepending SQL with Tangram lineage info: {prepend_str}")
         if isinstance(self.sql, str):
             self.sql = prepend_str + self.sql
