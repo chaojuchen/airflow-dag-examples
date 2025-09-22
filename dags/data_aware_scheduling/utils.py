b2b6bb0c969d6ec8eb45d8a302808c091f225e8d
chaojuchen <chaoju_chen@tangram-os.ai>
2025-09-22 11:36:06 +0200

    Fix the prepend_with_newline function

--- dags/data_aware_scheduling/utils.py
+++ dags/data_aware_scheduling/utils.py
@@ -5,14 +5,11 @@ class TangramSQLExecutionOperator(SQLExecuteQueryOperator):
     def __init__(self, *args, tangram_workspace, **kwargs):
         super().__init__(*args, **kwargs)
 
-        def prepend_with_newline(sql):
+        def prepend_with_newline(prepend_str, sql):
             return prepend_str + '\n' + sql
 
         prepend_str = f'-- {{"job":{{"rn":{{"resourceType":{{"app":{{"group":"org.apache","name":"airflow"}},"name":"TaskInstance"}},"names":["{tangram_workspace}", "{{{{ti.dag_id}}}}","{{{{ti.run_id}}}}","{{{{ti.task_id}}}}"]}}}}}}'
         prepend_func = functools.partial(prepend_with_newline, prepend_str)
         print(f"Prepending SQL with Tangram lineage info: {prepend_str}")
 
-        if isinstance(self.sql, str):
-            self.sql = prepend_func(self.sql)
-        elif isinstance(self.sql, list):
-            self.sql = [prepend_func(s) for s in self.sql]
+        self.sql = prepend_func(self.sql) if isinstance(self.sql, str) else [prepend_func(s) for s in self.sql]
